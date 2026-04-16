5a6153d0ec34afe9f6883ca37a953fb77225bf0b
RMadya <kereyoben75@gmail.com>
2026-04-16 11:19:29 +0700

    seharusnya password ga ada disitu, tapi nanti akan d pindah ke qpp config trus tinggal melakukan pemanggilan aja yang ada di DbConnection.cs. agar password lebih aman

--- MiMultyCBGApp/DAL/DbConnection.cs
+++ MiMultyCBGApp/DAL/DbConnection.cs
@@ -4,7 +4,7 @@ namespace MiMultyCBGApp.DAL
 {
     public class DbConnection
     {
-        // PENTING: Ganti "localhost" atau ".\SQLEXPRESS" sesuai nama Server SQL lokal.
+
         private static string connectionString = @"Server=NALZ\MUHAMMADBAGAS;Database=MIMultiCBGDB;User ID=sa;Password=Ragehaste90;Connection Timeout=5;";
 
         public static SqlConnection GetConnection()
